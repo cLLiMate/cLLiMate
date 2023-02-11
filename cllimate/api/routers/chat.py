eedd69e21adb00de732182ba3a86fbc1451c2502
Rithwik Seth <rithwikseth@gmail.com>
2023-02-11 17:44:45 -0500

    twilio and Fast update

--- cllimate/api/routers/chat.py
+++ cllimate/api/routers/chat.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Form, Response
+from fastapi import APIRouter, Form, Response
 from twilio.twiml.messaging_response import MessagingResponse
 
-app = FastAPI()
+router = APIRouter()
 
 
-@app.post("/hook")
-async def chat(From: str = Form(...), Body: str = Form(...)):
-   response = MessagingResponse() 
-   msg = response.message(f"Hi {From}, you said: {Body}")
-   return Response(content=str(response), media_type="application/xml")
+@router.get("/chat")
+def chat(From: str = Form(...), Body: str = Form(...)):
+    response = MessagingResponse()
+    msg = response.message(f"Hi {From}, you said: {Body}")
+    return Response(content=str(response), media_type="application/xml")
