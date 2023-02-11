ab53e8f9d7803a68ad3c46fa7a31066731f65343
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-02-11 05:05:32 -0500

    Deploy the backend api service

--- conf/terraform/site.tf
+++ conf/terraform/site.tf
@@ -20,7 +20,29 @@ resource "google_cloud_run_v2_service" "default" {
   depends_on = [google_project_service.default["run"]]
 }
 
-resource "google_cloud_run_service_iam_member" "all-users" {
+resource "google_cloud_run_v2_service" "api" {
+  for_each = toset(["live"])
+  name     = "${local.project_id}-api-${each.key}"
+  location = local.region
+
+  template {
+    scaling {
+      max_instance_count = 20
+    }
+    containers {
+      image = "${local.region}-docker.pkg.dev/${local.project_id}/${local.project_id}/api:${each.key}"
+    }
+  }
+
+  traffic {
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
+
+  depends_on = [google_project_service.default["run"]]
+}
+
+resource "google_cloud_run_service_iam_member" "default-all-users" {
   for_each = toset(keys(google_cloud_run_v2_service.default))
   service  = google_cloud_run_v2_service.default[each.key].name
   location = google_cloud_run_v2_service.default[each.key].location
@@ -28,9 +50,21 @@ resource "google_cloud_run_service_iam_member" "all-users" {
   member   = "allUsers"
 }
 
+resource "google_cloud_run_service_iam_member" "api-all-users" {
+  for_each = toset(keys(google_cloud_run_v2_service.api))
+  service  = google_cloud_run_v2_service.api[each.key].name
+  location = google_cloud_run_v2_service.api[each.key].location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
 output "service_url" {
-  value = {
+  value = merge({
     for key in keys(google_cloud_run_v2_service.default) :
     key => google_cloud_run_v2_service.default[key].uri
-  }
+  },
+  {
+    for key in keys(google_cloud_run_v2_service.api) :
+    "api-${key}" => google_cloud_run_v2_service.api[key].uri
+  })
 }
